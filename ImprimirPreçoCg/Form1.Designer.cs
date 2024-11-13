1378f0c2f80dfb4e294502db9317f1057e06bb58
felipe.silva <felipe.silva@cmrcia.com.br>
2024-11-13 09:52:13 -0300

    first commit

---
+++ ImprimirPreçoCg/Form1.Designer.cs
@@ -0,0 +1,77 @@
+﻿namespace ImprimirPreçoCg
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnArquivo = new System.Windows.Forms.Button();
+            this.btnImprimir = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnArquivo
+            // 
+            this.btnArquivo.Font = new System.Drawing.Font("Arial", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnArquivo.Location = new System.Drawing.Point(72, 142);
+            this.btnArquivo.Name = "btnArquivo";
+            this.btnArquivo.Size = new System.Drawing.Size(200, 72);
+            this.btnArquivo.TabIndex = 0;
+            this.btnArquivo.Text = "Selecionar Arquivo";
+            this.btnArquivo.UseVisualStyleBackColor = true;
+            this.btnArquivo.Click += new System.EventHandler(this.btnArquivo_Click);
+            // 
+            // btnImprimir
+            // 
+            this.btnImprimir.Font = new System.Drawing.Font("Arial", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnImprimir.Location = new System.Drawing.Point(352, 142);
+            this.btnImprimir.Name = "btnImprimir";
+            this.btnImprimir.Size = new System.Drawing.Size(200, 72);
+            this.btnImprimir.TabIndex = 1;
+            this.btnImprimir.Text = "Imprimir";
+            this.btnImprimir.UseVisualStyleBackColor = true;
+            this.btnImprimir.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(210)))), ((int)(((byte)(210)))), ((int)(((byte)(210)))));
+            this.ClientSize = new System.Drawing.Size(634, 369);
+            this.Controls.Add(this.btnImprimir);
+            this.Controls.Add(this.btnArquivo);
+            this.Name = "Form1";
+            this.Text = "Imprimir ";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnArquivo;
+        private System.Windows.Forms.Button btnImprimir;
+    }
+}
+
---
+++ ImprimirPreçoCg/Form1.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Printing;
+using System.IO;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ImprimirPreçoCg
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        string linha;
+        string stringCompleta;
+        public void imprimir()
+        {
+            string zplCommand = stringCompleta;
+
+            string printerIpAddress = "10.40.2.104"; // Substitua pelo endereço IP da impressora Zebra na rede
+
+            //// Crie uma instância do PrintDialog
+            //PrintDialog printDialog = new PrintDialog();
+
+            //// Exiba o diálogo de impressão
+            //DialogResult result = printDialog.ShowDialog();
+
+            //string printerIp = Convert.ToString(printDialog.PrinterSettings.PrinterName);
+            try
+            {
+                using (TcpClient client = new TcpClient(printerIpAddress, 9100))
+                {
+                    using (NetworkStream stream = client.GetStream())
+                    {
+                        byte[] buffer = Encoding.ASCII.GetBytes(zplCommand);
+                        stream.Write(buffer, 0, buffer.Length);
+
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Erro ao enviar o comando ZPL: {ex.Message}");
+            }
+        }
+
+        private void btnArquivo_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog paste = new OpenFileDialog();
+
+
+            paste.Filter = "All files (*.txt)|*.txt";
+            paste.Title = "Selecione o arquivo";
+            paste.ShowDialog();
+
+            string csv = paste.FileName;
+
+            StreamReader lerArquivo = new StreamReader(File.OpenRead(csv));
+
+
+            while ((linha = lerArquivo.ReadLine()) != null)
+            {
+                stringCompleta = stringCompleta + linha;
+            }            
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            imprimir();
+        }
+    }
+}
